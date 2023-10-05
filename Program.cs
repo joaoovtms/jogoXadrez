3f89d52f1b685d3558734126ffed5d101928d18a
joaoovtms <joaovictormoreirasarmento@gmail.com>
2023-10-05 16:20:16 -0300

    Criando primeiras classes e implementado tela(tabuleiro)

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ namespace JogoXadrez
     {
         static void Main(string[] args)
         {
-            Posicao p = new Posicao(3, 4);
-            Console.WriteLine(p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
         }
     }
 }
