4d1116259b2d0c023e41264022a35f1b8a66a57c
joaoovtms <joaovictormoreirasarmento@gmail.com>
2023-10-06 12:00:50 -0300

    Testes iniciais com movimentos possiveis das pecas

--- Program.cs
+++ Program.cs
@@ -20,7 +20,14 @@ namespace JogoXadrez
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
-                    Console.WriteLine("Destino: ");
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
+                    Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
                     partida.executarMovimento(origem, destino);
